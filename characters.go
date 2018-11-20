5d2842b893bf5fe0a3bc66327428e7054789edba
Adrian Johnson <zephyr1914@gmail.com>
2018-11-20 08:39:57 -0500

    update package path

--- characters.go
+++ characters.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/adrianjjohnson/gomarvel/model"
+	"github.com/johnsonaj/gomarvel/model"
 )
 
 // GetAllCharacters returns a list of character results
--- examples/characters/main.go
+++ examples/characters/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/adrianjjohnson/gomarvel"
+	"github.com/johnsonaj/gomarvel"
 )
 
 func main() {
